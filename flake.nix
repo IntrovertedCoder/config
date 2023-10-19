999982a3d5bef665543f9ac30e782e5698e28160
IntrovertedCoder <natrotnic@gmail.com>
2023-10-19 15:40:56 -0500

    Testing

---
+++ flake.nix
@@ -0,0 +1,148 @@
+{
+  description = "flake for aLaptop";
+
+  inputs = {
+    # NixPkgs (nixos-22.11)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    # NixPkgs Unstable (nixos-unstable)
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home Manager (release-22.05)
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # macOS Support (master)
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Hardware Configuration
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+
+    # Generate System Images
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Snowfall Lib
+    snowfall-lib.url = "github:snowfallorg/lib/dev";
+    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Snowfall Flake
+    flake.url = "github:snowfallorg/flake";
+    flake.inputs.nixpkgs.follows = "unstable";
+
+    # Comma
+    comma.url = "github:nix-community/comma";
+    comma.inputs.nixpkgs.follows = "unstable";
+
+    # System Deployment
+    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.nixpkgs.follows = "unstable";
+
+    # Run unpatched dynamically compiled binaries
+    nix-ld.url = "github:Mic92/nix-ld";
+    nix-ld.inputs.nixpkgs.follows = "unstable";
+
+    # Binary Cache
+    attic = {
+      url = "github:zhaofengli/attic";
+
+      # @FIXME(jakehamilton): A specific version of Rust is needed right now or
+      # the build fails. Re-enable this after some time has passed.
+      inputs.nixpkgs.follows = "unstable";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+
+    # Vault Integration
+    vault-service = {
+      url = "github:DeterminateSystems/nixos-vault-service";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Flake Hygiene
+    flake-checker = {
+      url = "github:DeterminateSystems/flake-checker";
+      inputs.nixpkgs.follows = "unstable";
+    };
+
+    # Cows!
+    cowsay = {
+      url = "github:snowfallorg/cowsay";
+      # @NOTE(jakehamilton): A recent version of VHS currently fails
+      # causes cow2img to fail. This needs to be fixed upstream:
+      # https://github.com/charmbracelet/vhs/issues/361
+
+      # inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.unstable.follows = "unstable";
+    };
+
+    # Backup management
+    icehouse = {
+      url = "github:snowfallorg/icehouse";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.unstable.follows = "unstable";
+    };
+
+    # Yubikey Guide
+    yubikey-guide = {
+      url = "github:drduh/YubiKey-Guide";
+      flake = false;
+    };
+
+    # GPG default configuration
+    gpg-base-conf = {
+      url = "github:drduh/config";
+      flake = false;
+    };
+
+    bibata-cursors = {
+      url = "github:suchipi/Bibata_Cursor";
+      flake = false;
+    };
+  };
+
+  outputs = inputs: let
+    lib = inputs.snowfall-lib.mkLib {
+      inherit inputs;
+      src = ./.;
+
+      snowfall = {
+        meta = {
+          name = "plusultra";
+          title = "Plus Ultra";
+        };
+
+        namespace = "plusultra";
+      };
+    };
+  in
+    lib.mkFlake {
+      channels-config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "electron-20.3.11"
+          "nodejs-16.20.0"
+          "python-2.7.18.6"
+        ];
+      };
+
+      overlays = with inputs; [
+      ];
+      systems.modules.nixos = with inputs; [
+        home-manager.nixosModules.home-manager
+        nix-ld.nixosModules.nix-ld
+        vault-service.nixosModules.nixos-vault-service
+      ];
+
+    };
+}
+  # outputs = { self, nixpkgs }: {
+    # nixosConfigurations = {
+      # aLaptop = nixpkgs.lib.nixosSystem {
+        # system = "x86_64-linux";
+        # modules = [
+          # /etc/nixos/configuration.nix
+        # ];
+      # };
+    # };
+  # };
