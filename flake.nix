b39af8da661bcdd4365f4308daae58e558ea091e
IntrovertedCoder <natrotnic@gmail.com>
2023-10-20 05:56:30 -0500

    This is working currently with my old config.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "flake for aLaptop";
 
   inputs = {
-    # NixPkgs (nixos-22.11)
+    # NixPkgs (nixos-23.05)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
     # NixPkgs Unstable (nixos-unstable)
