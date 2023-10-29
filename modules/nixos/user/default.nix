14fc02d2c702f62cf6c38a997c2ae44761cb50c9
IntrovertedCoder <natrotnic@gmail.com>
2023-10-29 14:33:00 -0500

    Wrong shell (still need to configure this file)

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -133,7 +133,7 @@ in
       home = "/home/${cfg.name}";
       group = "users";
 
-      shell = pkgs.zsh;
+      shell = pkgs.fish;
 
       # Arbitrary user ID to use for the user. Since I only
       # have a single user on my machines this won't ever collide.
