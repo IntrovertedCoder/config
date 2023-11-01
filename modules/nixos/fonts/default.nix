368333b97312a25b09417ec08615ec6aa9ab9eae
IntrovertedCoder <natrotnic@gmail.com>
2023-11-01 18:35:17 -0500

    I think moved font out to its own file

---
+++ modules/nixos/fonts/default.nix
@@ -0,0 +1,12 @@
+{ options, config, pkgs, lib, ... }:
+
+with lib;
+with lib.plusultra;
+let
+  cfg = config.plusultra.font;
+in
+{
+  options.plusultra.font = with types; {
+    Font = mkOpt str "Hack Regular Nerd Font Complete Mono" "Font";
+  };
+}
