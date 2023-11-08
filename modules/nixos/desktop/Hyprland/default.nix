0b0ab1e1ed63328810e8acba98cce6ac26d84d02
IntrovertedCoder <natrotnic@gmail.com>
2023-11-08 10:13:09 -0600

    Fixed some minor things in Hyprland

--- modules/nixos/desktop/Hyprland/default.nix
+++ modules/nixos/desktop/Hyprland/default.nix
@@ -12,10 +12,10 @@ let
 in
 {
   options.plusultra.desktop.Hyprland= with types; {
-    enable = mkBoolOpt false "Whether or not to enable Sway.";
+    enable = mkBoolOpt false "Whether or not to enable Hyprland.";
     wallpaper = mkOpt (nullOr package) null "The wallpaper to display.";
     extraConfig =
-      mkOpt str "" "Additional configuration for the Sway config file.";
+      mkOpt str "" "Additional configuration for the Hyprland config file.";
   };
 
   config = mkIf cfg.enable {
@@ -23,91 +23,13 @@ in
     plusultra.apps = {
       home.mako.enable = true;
     };
-    # Desktop additions
-    # plusultra.desktop.addons = {
-      # gtk = enabled;
-      # foot = enabled;
-      # mako = enabled;
-      # rofi = enabled;
-      # wofi = enabled;
-      # swappy = enabled;
-      # kanshi = enabled;
-      # waybar = enabled;
-      # keyring = enabled;
-      # nautilus = enabled;
-      # xdg-portal = enabled;
-      # electron-support = enabled;
-    # };
 
-    plusultra.home.configFile."foobar".text = ''
-      the quick brown fox jumped over the lazy dog
-    '';
+    # Adding the hyprland config
     plusultra.home.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
 
-    # programs.sway = {
-      # enable = true;
-      # extraPackages = with pkgs; [
-        # rofi
-        # swaylock
-        # swayidle
-        # gnome.gnome-control-center
-      # ];
-
-      # extraSessionCommands = ''
-        # export SDL_VIDEODRIVER=wayland
-        # export QT_QPA_PLATFORM=wayland
-        # export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-        # export _JAVA_AWT_WM_NONREPARENTING=1
-        # export MOZ_ENABLE_WAYLAND=1
-        # export XDG_SESSION_TYPE=wayland
-        # export XDG_SESSION_DESKTOP=sway
-        # export XDG_CURRENT_DESKTOP=sway
-      # '';
-    # };
-
-    # wayland.windowManager.hyprland = {
-      # enable = true;
-      # systemdIntegration = true;
-      # xwayland.enable = true;
-    # };
     programs.hyprland = {
       enable = true;
       xwayland.enable = true;
     };
-
-    # configuring sway itself (assmung a display manager starts it)
-    # systemd.user.targets.sway-session = {
-      # description = "Sway compositor session";
-      # documentation = [ "man:systemd.special(7)" ];
-      # bindsTo = [ "graphical-session.target" ];
-      # wants = [ "graphical-session-pre.target" ];
-      # after = [ "graphical-session-pre.target" ];
-    # };
-
-    # systemd.user.services.sway = {
-      # description = "Sway - Wayland window manager";
-      # documentation = [ "man:sway(5)" ];
-      # bindsTo = [ "graphical-session.target" ];
-      # wants = [ "graphical-session-pre.target" ];
-      # after = [ "graphical-session-pre.target" ];
-      # # We explicitly unset PATH here, as we want it to be set by
-      # # systemctl --user import-environment in startsway
-      # environment.PATH = lib.mkForce null;
-      # serviceConfig = {
-        # Type = "simple";
-        # ExecStart = ''
-          # ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
-        # '';
-        # Restart = "on-failure";
-        # RestartSec = 1;
-        # TimeoutStopSec = 10;
-      # };
-    # };
-
-    # services.xserver.enable = true;
-    # services.xserver.displayManager.defaultSession = "sway";
-    # services.xserver.displayManager.gdm.enable = true;
-    # services.xserver.displayManager.gdm.wayland = true;
-    # services.xserver.libinput.enable = true;
   };
 }
