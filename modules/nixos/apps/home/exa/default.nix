3fc1a26127434c9e65500cde51b7801bd8661304
IntrovertedCoder <natrotnic@gmail.com>
2023-11-07 21:56:10 -0600

    For some reason fish.functions wasn't working

--- modules/nixos/apps/home/exa/default.nix
+++ modules/nixos/apps/home/exa/default.nix
@@ -14,7 +14,7 @@ in
       exa = {
         enable = true;
       };
-      fish.functions = mkIf config.plusultra.apps.home.fish.enable {
+      fish.shellAliases= mkIf config.plusultra.apps.home.fish.enable {
         ls = "exa -ax --icons --git $argv";
         ll = "exa -alhg --icons --git --octal-permissions $argv";
         tree = "exa -a --icons --tree --level 3 -F -I='.git.' --color=always $argv | less -r +g";
