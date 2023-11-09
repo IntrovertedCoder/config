ce9d91cdedf64e3e2631c8a14e1aed09a56b7eda
IntrovertedCoder <natrotnic@gmail.com>
2023-11-09 17:50:14 -0600

    Removed NUR from configuration.nix

--- systems/x86_64-linux/aLaptop/configuration.nix
+++ systems/x86_64-linux/aLaptop/configuration.nix
@@ -5,11 +5,6 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
   imports =
     [ # Include the results of the hardware scan.
       # ./hardware-configuration.nix
