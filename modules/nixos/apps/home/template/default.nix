febd89c78caf29178b85a1a631ba5e900fc57d36
IntrovertedCoder <natrotnic@gmail.com>
2023-11-16 14:49:20 -0600

    Added apps.home template

---
+++ modules/nixos/apps/home/template/default.nix
@@ -0,0 +1,15 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.plusultra;
+let cfg = config.plusultra.apps.home.template;
+in
+{
+  options.plusultra.apps.home.template = with types; {
+    enable = mkBoolOpt false "Whether or not to enable template.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ template ];
+  };
+}
