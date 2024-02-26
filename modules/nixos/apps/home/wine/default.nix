eaf33c1ac5f2eac2248915c1b09dd3838cdf7f3d
IntrovertedCoder <NATrotnic@gmail.com>
2024-02-26 10:42:12 -0600

    Adding wine to suite gaming

---
+++ modules/nixos/apps/home/wine/default.nix
@@ -0,0 +1,18 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.plusultra;
+let cfg = config.plusultra.apps.home.wine;
+in
+{
+  options.plusultra.apps.home.wine = with types; {
+    enable = mkBoolOpt false "Whether or not to enable wine.";
+  };
+
+  config = mkIf cfg.enable {
+    # environment.systemPackages = with pkgs; [ wine ];
+    plusultra.home.extraOptions.home.packages = with pkgs; [
+      winetricks
+    ];
+  };
+}
--- modules/nixos/suites/gaming/default.nix
+++ modules/nixos/suites/gaming/default.nix
@@ -18,6 +18,7 @@ in
           heroic = enabled;
           mangohud = enabled;
           lutris = enabled;
+          wine = enabled;
         };
       };
     };
