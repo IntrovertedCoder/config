15eadc6485c114439b7f17c6388d52cb89b22cc5
IntrovertedCoder <NATrotnic@gmail.com>
2024-05-30 22:58:17 -0500

    Needed for home-manager for some reason

--- systems/x86_64-linux/Iridium/default.nix
+++ systems/x86_64-linux/Iridium/default.nix
@@ -85,6 +85,8 @@ with lib.plusultra;
     ];
   };
 
+  programs.dconf.enable = true;
+
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
