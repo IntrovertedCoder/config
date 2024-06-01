cc58e93ddc61a70ac0126ca86379352fa304c13d
IntrovertedCoder <NATrotnic@gmail.com>
2024-06-01 17:01:08 -0500

    Modifying the containerTemplate to make it easier

--- modules/nixos/apps/server/containerTemplate/default.nix
+++ modules/nixos/apps/server/containerTemplate/default.nix
@@ -7,19 +7,31 @@ in
 {
   options.plusultra.apps.server.containerTemplate = with types; {
     enable = mkBoolOpt false "Whether or not to enable containerTemplate container.";
+    network = mkOpt str "" "The network id to put this container in.";
+    ip = mkOpt str "" "The ip address to apply to the container.";
   };
 
   config = mkIf cfg.enable {
+    system.activationScripts.piholeFolder = ''
+      mkdir /data >/dev/null 2>&1 || true
+      mkdir /data/containerTemplate >/dev/null 2>&1 || true
+    '';
     virtualisation.oci-containers.containers = {
       containerTemplate = {
-        image = "owner/containerTemplate:version";
+        image = "containerTemplate/containerTemplate:version";
         hostname = "containerTemplate";
         autoStart = true;
-        ports = [
-          "externalPort:internalPort"
+        # Can be any ip between 172.77.0.1 - 172.77.255.254
+        extraOptions = [
+          "--network=${cfg.network}"
+          "--ip=${cfg.ip}"
         ];
-        enviornment = {
+        ports = [
+          # "externalPort:internalPort";
+        ];
+        environment = {
           # variable = "string";
+          TZ = "${config.time.timeZone}";
         };
         volumes = [
           # "externalLocation:internalLocation"
