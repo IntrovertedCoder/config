0269939a86f6715fe66221971b3a5f865bcc8dc3
IntrovertedCoder <NATrotnic@gmail.com>
2025-10-17 21:32:21 -0600

    Moving and changing tz for aLaptop

--- systems/x86_64-linux/aLaptop/configuration.nix
+++ systems/x86_64-linux/aLaptop/configuration.nix
@@ -12,9 +12,6 @@
 
   networking.hostName = "aLaptop"; # Define your hostname.
 
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
